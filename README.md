cc582b2a74e5c57231da6b3af23a3cf6fb9127e7
肖一剑 <xiaoyijian@xueleyun.com>
2016-08-02 13:43:23 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # coreJava
+## test
---
+++ src/main/java/Test.java
@@ -0,0 +1,8 @@
+/**
+ * Created by xiaoyijian on 2016/8/2.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
